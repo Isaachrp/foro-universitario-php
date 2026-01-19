d0aab01acaa025ff773f503c5ae0f8aadb9b5af1
Isaac R <isaac040124@gmail.com>
2026-01-18 22:31:51 -0600

    Autenticación completa; sesión activa; botón eliminar pendiente

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/../app/helpers/security.php';
+
+
 
 session_set_cookie_params([
     'lifetime' => 0,
@@ -16,5 +17,5 @@ if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 */
-
+require_once __DIR__ . '/../app/helpers/security.php';
 require_once __DIR__ . '/../routes/web.php';
