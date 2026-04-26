9ad58e208fb9fc6dade641b4f5fd9773d0feb144
Isaac Rios <isaac040124@gmail.com>
2026-04-25 23:41:05 -0600

    Remove
     security.

--- app/helpers/mail.php
+++ app/helpers/mail.php
@@ -11,12 +11,12 @@ function sendMail($to, $subject, $body)
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'eforum05@gmail.com';
-        $mail->Password   = 'airs xeiw tqrs caho';
+        $mail->Username   = '';
+        $mail->Password   = '';
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
-        $mail->setFrom('eforum05@gmail.com', 'Foro Universitario');
+        $mail->setFrom('', 'Foro Universitario');
         $mail->addAddress($to);
 
         $mail->isHTML(true);
